f07e79ba7df9c549a2c3fa4e5ff499d3b691cb9f
Jorge Ferrero Linacero <jorgeferrerolinacero@gmail.com>
2024-05-30 02:24:54 +0200

    Update github.go

--- pkg/github/github.go
+++ pkg/github/github.go
@@ -1,2 +1,6 @@
-// Package github provides a Go API.
+// Package github provides a set of authentication methods for the GitHub API.
+// This package is a wrapper around the github.com/google/go-github to provide
+// a simple way to authenticate with the GitHub API using TokenSource interface
+// as golang.org/x/oauth2 does.
+// This package is able to authenticate GitHub Apps and Installations.
 package github
